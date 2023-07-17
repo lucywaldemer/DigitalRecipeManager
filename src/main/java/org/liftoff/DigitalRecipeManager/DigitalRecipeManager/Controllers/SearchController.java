3c5422bd645833aa3d3a7093d01fec5430109f83
helenWIP <helenfrancesjones@gmail.com>
2023-07-17 17:00:43 -0500

    updated controllers and models

---
+++ src/main/java/org/liftoff/DigitalRecipeManager/DigitalRecipeManager/Controllers/SearchController.java
@@ -0,0 +1,41 @@
+package org.liftoff.DigitalRecipeManager.DigitalRecipeManager.Controllers;
+
+import org.liftoff.DigitalRecipeManager.DigitalRecipeManager.Models.Recipe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("search")
+public class SearchController {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    @GetMapping("")
+    public String search(Model model) {
+        model.addAttribute("categories", RecipeData.getAllCategories());
+        return "search";
+    }
+
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String category) {
+        List<Recipe> recipes;
+        if (category.equalsIgnoreCase("all")) {
+            recipes = recipeRepository.findAll();
+        } else {
+            recipes = recipeRepository.findByCategory(category);
+        }
+        model.addAttribute("categories", RecipeData.getAllCategories());
+        model.addAttribute("title", "Recipes in Category: " + category);
+        model.addAttribute("recipes", recipes);
+
+        return "search";
+    }
+}
