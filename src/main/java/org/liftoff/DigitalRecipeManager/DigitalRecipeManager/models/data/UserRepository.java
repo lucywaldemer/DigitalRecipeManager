8721f9260a1722c43a4ac49783cd50a9159a307e
DaddyLongLegs <blspiecker95@gmail.com>
2023-07-18 10:31:04 -0500

    Added Templates For Register and Login

--- src/main/java/org/liftoff/DigitalRecipeManager/DigitalRecipeManager/models/data/UserRepository.java
+++ src/main/java/org/liftoff/DigitalRecipeManager/DigitalRecipeManager/models/data/UserRepository.java
@@ -6,7 +6,4 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     User findByUsername(String username);
-    Optional<User> findById(Integer userId) {
-       return findById(userId);
-   }
 }
