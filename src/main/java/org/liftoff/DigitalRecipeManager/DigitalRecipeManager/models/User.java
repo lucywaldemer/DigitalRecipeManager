af4d78ea890540a8cfa5c2ed4cd0fb36af788672
Lu Waldemer <lucywaldemer@gmail.com>
2023-07-20 20:21:56 -0500

    updating project so we can merge July 20th

--- src/main/java/org/liftoff/DigitalRecipeManager/DigitalRecipeManager/models/User.java
+++ src/main/java/org/liftoff/DigitalRecipeManager/DigitalRecipeManager/models/User.java
@@ -1,17 +1,19 @@
 package org.liftoff.DigitalRecipeManager.DigitalRecipeManager.models;
 
 import com.sun.istack.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import javax.persistence.*;
 
 @Entity
 // stores a file name for photos
 @Table(name = "users")
 public class User extends AbstractEntity {
+    // generates a user id for each user
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // needs getters and setters
 // stores the photo containing up to 64 characters
     @Column(nullable = true, length = 64)
     private String photos;
@@ -28,6 +30,19 @@ public class User extends AbstractEntity {
 
     @NotNull
     private String pwHash;
+
+    // helps the password encoded get recognized by the client
+    @Autowired
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+// This was from https://stackoverflow.com/questions/43007763/spring-security-encoded-password-gives-me-bad-credentials
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder());
+    }
 
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
