188d7b0a36d6ccbb3483a7a3b328c4f13bdfe015
senayinan <senayinan@live.com>
2023-08-16 15:56:20 -0500

    bootstrap and view added

--- src/main/java/org/liftoff/DigitalRecipeManager/DigitalRecipeManager/controllers/IngredientController.java
+++ src/main/java/org/liftoff/DigitalRecipeManager/DigitalRecipeManager/controllers/IngredientController.java
@@ -1,8 +1,7 @@
 package org.liftoff.DigitalRecipeManager.DigitalRecipeManager.controllers;
 
 import org.liftoff.DigitalRecipeManager.DigitalRecipeManager.data.IngredientRepository;
-import org.liftoff.DigitalRecipeManager.DigitalRecipeManager.models.Ingredient;
-import org.liftoff.DigitalRecipeManager.DigitalRecipeManager.models.RecipeService;
+import org.liftoff.DigitalRecipeManager.DigitalRecipeManager.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 
-@RestController
+@Controller
 @RequestMapping("ingredients")
 public class IngredientController {
 
@@ -25,11 +24,45 @@ public class IngredientController {
         return recipeService.findAllIngredients();
     }
     @GetMapping("add")
+
     public String displayAddIngredientForm(Model model) {
         model.addAttribute("title", "Add Ingredient");
         model.addAttribute(new Ingredient());
+        model.addAttribute("ingredients", ingredientRepository.findAll());
 
         return "ingredients/add";
+    }
+
+    @GetMapping
+    public String displayIngredients(Model model) {
+        model.addAttribute("title", "Ingredients");
+
+        model.addAttribute("ingredients", ingredientRepository.findAll());
+
+        return "ingredients/index";
+    }
+
+    @GetMapping("edit/{ingredientId}")
+    public String displayEditIngredientForm(Model model, @PathVariable int ingredientId) {
+        Ingredient ingredientToEdit = ingredientRepository.findById(ingredientId);
+        String title = "Edit Recipe " + ingredientToEdit.getName() + " (id=" + ingredientToEdit.getId() + ")";
+        model.addAttribute("title", title );
+        model.addAttribute("ingredient", ingredientToEdit);
+
+        return "ingredients/edit";
+    }
+
+    @PostMapping("edit")
+    public String processEditIngredientForm(int ingredientId, String name) {
+
+        Ingredient ingredientToEdit = ingredientRepository.findById(ingredientId);
+
+        ingredientToEdit.setName(name);
+
+
+        ingredientRepository.save(ingredientToEdit);
+
+        return "redirect:";
     }
 
     @PostMapping("add")
