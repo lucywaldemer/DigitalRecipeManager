dc757998df22329613cb10128c8648e4511f8b95
Lu Waldemer <lucywaldemer@gmail.com>
2023-08-16 18:16:28 -0500

    updated code 6pm

--- src/main/java/org/liftoff/DigitalRecipeManager/DigitalRecipeManager/models/SecurityConfig.java
+++ src/main/java/org/liftoff/DigitalRecipeManager/DigitalRecipeManager/models/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers( "/register").permitAll() // Whitelist the register page for unauthenticated access
+                .antMatchers("/styles/**").permitAll()
                 .anyRequest().authenticated() // All other requests require authentication
                 .and()
                 .formLogin()
