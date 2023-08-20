886b4645c48eb1dc6af637dc9d5d7aa987b2771d
dominicgoldman@yahoo.com <dominicgoldman@yahoo.com>
2023-08-20 14:15:47 -0500

    finished files

---
+++ src/main/java/org/liftoff/DigitalRecipeManager/DigitalRecipeManager/models/RecipeResponse.java
@@ -0,0 +1,18 @@
+package org.liftoff.DigitalRecipeManager.DigitalRecipeManager.models;
+
+import java.util.List;
+
+public class RecipeResponse {
+    private List<Recipe> meals;
+
+    public List<Recipe> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Recipe> meals) {
+        this.meals = meals;
+    }
+}
+
+
+
